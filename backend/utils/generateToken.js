c2c2d9a30b65c740f5a17167e6bc727c35c6e946
Shiwang007 <shiwangkush007@gmail.com>
2024-08-02 23:48:59 +0530

    updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,6 @@ const generateToken = (userId, res) => {
         maxAge: 1 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'strict',
-        secure: process.env.NODE_ENV !== 'development'
     } )
 }
 
