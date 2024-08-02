99884ac7bc28fc16257495fd3d30f9cea437c7f1
Shiwang007 <shiwangkush007@gmail.com>
2024-08-02 22:45:53 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,22 @@ const userRoutes = require('./routes/user.routes')
 const messageRoutes = require('./routes/message.routes')
 const mongodbConnect = require("./db/connectToMongodb");
 const { app, server } = require('./socket/socket');
-// const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({
-//     origin: "http://localhost:3000",
-//     credentials: true
-// }))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../",  "frontend", "dist", "index.html"));
+})
 
 server.listen(process.env.PORT || 5000, () => {
     console.log(`listening on port ${process.env.PORT || 5000} `)
